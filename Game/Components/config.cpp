4ae93502765cd909f7513d8c6ed5af6d0f0c51e6
DynamiteDunn <coderdunn91@hotmail.com>
2012-11-19 01:29:34 -0800

    General fixes, attempted to add helpfull "To-Do" comments
    Added empty "StringSplit" function at the bottom as a starting point,
    edited the parseLine function to implement StringSplit.

--- Game/Components/config.cpp
+++ Game/Components/config.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-using std::cout;
-using std::string;
-
 #include "config.h"
+
+using namespace std;
 using namespace JAMM;
+
+// Forward function declaration, implementation is below
+bool StringSplit(const string &input, char delimiter, string &result1, string &result2);
+
+// ================ ConfigFile class members ================
 
 ConfigFile::ConfigFile() // Default constructor
 {
@@ -19,11 +23,10 @@ void ConfigFile::parseFile() // Loops through the file and adds contents to data
 	// load the file
 	configFile.open(configFileName);
 
-	// The problem occurs somewhere around here.
-
 	if (!configFile.good())
 	{
 		cout << "Error: Could not load configuration file.\n";
+        return;
 	}
 
 	while (!configFile.eof())
@@ -35,22 +38,50 @@ void ConfigFile::parseFile() // Loops through the file and adds contents to data
 
 void ConfigFile::parseLine(const string &line)
 {
-	// Depends on config file with complete integrity; no errors can be present.
-	// Needs to be refined.
+    // Break out of function if line is blank
+    if (line.length() == 0)
+        return;
 
 	// Variables
-	size_t pos; // to be used by string.find()
-	string key, value; // data to be pushed to data map
+	string key(""), value(""); // data to be pushed to data map, intialized with empty strings
 
-	pos = line.find("=");
-	if (!pos) // If it can't find the = then it must be a bad file.
+    // Attempt to split the line
+	if (!StringSplit(line, '=', key, value))
 	{
-		cout << "Error: Invalid configuration file.\n";
+		cout << "Config file load Error, while parsing line: " + line;
+        return;
 	}
-	
-	key = line.substr(0, pos-2);
-	value = line.substr(pos+2);
+
+    //  ==================== IMPORTANT ====================
+    //  It is important to strip the whitespace characters from the key and value pair!
+    //  So that should be done Here before adding the pair to the map.
+    //  ===================================================
 
 	data[key] = value;
 	return;
+}
+
+// ================ Helper Functions ================
+
+// If you can't find a standard library function, do it yourself!
+// Notice that this function isn't part of the "ConfigFile" class, and that's okay! It's just local to this file.
+// We might want to move this later to a "StringTools.cpp" file if we find the need for this function elsewhere in the game,
+// or write additional helper functions.
+//
+// Anyway, this will take the "input" string, split it at the "delimiter" character, and put the 2 halves in "result1" and 
+// "result2", or return false if something goes wrong
+bool StringSplit(const string &input, char delimiter, string &result1, string &result2)
+{
+    // Set both result1 and result2 to blank strings
+    result1 = result2 = "";
+
+    // Check to make sure input is at least 1 character using "input.length()". If check fails, return false
+
+
+    // Loop through each charactor in "input", using "input[index]", and if the character is not equal to 'delimiter'
+    // then add the character to the end of "result1" via "result1.push_back(input[index])". If the character equals 
+    // 'delimiter', then skip that character, then continue adding the rest of the characters to 'result2' until you hit
+    // the end of the string.
+
+    return true;
 }
